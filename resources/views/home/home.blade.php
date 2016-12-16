0dfadae9fa82d12079d866542340e964288cde7e
Mauricio Costanzo <ventas@worldmaster.com.uy>
2016-12-16 16:22:37 -0300

    ok

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -36,60 +36,7 @@
 
 
 
-  <!-- SMOOTH SCROLL -->
-  <script>
-  $(function() {
-    // This will select everything with the class smoothScroll
-    // This should prevent problems with carousel, scrollspy, etc...
-    $('.smoothScroll').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 800); // The number here represents the speed of the scroll in milliseconds
-          return false;
-        }
-      }
-    });
-  });
-  </script>
-  <!-- End of SMOOTH SCROLL -->
 
-
-
-
-
-<!-- hidden bar to show on scroll -->
-<script>
-  $(window).scroll(function(e){
-    if ($(this).scrollTop() > 320) { // choose the value you want.
-        $('#mynavbar:hidden').slideDown("slow"); //you can use fadeIn();
-    } else {
-        $('#mynavbar:visible').slideUp('slow'); // you can use fadeOut();
-    }
-});
-</script>
-
-
-<!-- hidden #top button -->
-<script>
-  (function ($) {
-  $(document).ready(function(){
-  $(".navbar").hide();
-  $(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        $('.navbar').fadeIn();
-      } else {
-        $('.navbar').fadeOut();
-      }
-    });
-  });
-
-});
-</script>
 
 
 </head>
@@ -947,9 +894,7 @@ Ofrezco con experiencia y formación, la capacitación adecuada en cuanto a la e
   <!-- activate WOW.js (ya está cargada al principio del html code) --> 
   <script> new WOW().init(); </script>
 
-  <script src="js/main.js"></script> <!-- Back-to-Top Button / Gem jQuery -->
-  <script src="js/validator.js"></script><!-- form -->
-  <script src="js/contact.js"></script><!-- form -->
+ 
 
 <!-- mis nav-tabs que son divs en vez de ser ul y li //  funciona!! :D --> 
   <script>
@@ -958,6 +903,61 @@ Ofrezco con experiencia y formación, la capacitación adecuada en cuanto a la e
     $(this).addClass('active')
   })
   </script>
+
+    <!-- SMOOTH SCROLL -->
+  <script>
+  $(function() {
+    // This will select everything with the class smoothScroll
+    // This should prevent problems with carousel, scrollspy, etc...
+    $('.smoothScroll').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 800); // The number here represents the speed of the scroll in milliseconds
+          return false;
+        }
+      }
+    });
+  });
+  </script>
+  <!-- End of SMOOTH SCROLL -->
+
+
+
+
+
+<!-- hidden bar to show on scroll -->
+<script>
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 320) { // choose the value you want.
+        $('#mynavbar:hidden').slideDown("slow"); //you can use fadeIn();
+    } else {
+        $('#mynavbar:visible').slideUp('slow'); // you can use fadeOut();
+    }
+});
+</script>
+
+
+<!-- hidden #top button -->
+<script>
+  (function ($) {
+  $(document).ready(function(){
+  $(".navbar").hide();
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('.navbar').fadeIn();
+      } else {
+        $('.navbar').fadeOut();
+      }
+    });
+  });
+
+});
+</script>
 
 
   </body>
