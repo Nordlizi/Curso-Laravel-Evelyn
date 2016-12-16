420ade8bdc162c2fc4e8c9c1141f6720e678b121
Mauricio Costanzo <ventas@worldmaster.com.uy>
2016-12-16 19:50:34 -0300

    ok

--- resources/views/home/evelyn/formulario.blade.php
+++ resources/views/home/evelyn/formulario.blade.php
@@ -37,11 +37,11 @@
                     {!! Form::text('name', null ,['class'       => 'form-control',
                                                   'placeholder' => 'Escribe tu nombre aquí *' ,
                                                   'required'    => 'required',
-                                                  'data-error'  => 'Por favor, ingresa tu nombre.',
-                                                  'id'          => 'username']) !!}
-                    
+                                                  'data-error'  => 'Por favor, ingresa tu nombre.']) !!}
+
+                    <div class="help-block with-errors"></div>
                 </div>
-                <div class="help-block with-errors"></div>
+                
             </div>            
         </div>
         <div class="row">
@@ -52,9 +52,9 @@
                                                   'placeholder' => 'Escribe tu dirección de correo aquí *' ,
                                                   'required'    => 'required',
                                                   'data-error'  => 'Por favor, ingresa una dirección de correo válida.']) !!}
-                   
+                <div class="help-block with-errors"></div>   
                 </div>
-                <div class="help-block with-errors"></div>
+                
             </div>
             <div class="col-md-6">
                 <div class="form-group">                    
@@ -62,9 +62,9 @@
                     {!! Form::text('telefono', null ,['class'       => 'form-control',
                                                       'placeholder' => 'Escribe aquí tu número de línea o celular' ]) !!}
 
-                    
+                 <div class="help-block with-errors"></div>   
                 </div>
-                <div class="help-block with-errors"></div>
+                
             </div>
         </div>
         <div class="row">
@@ -76,9 +76,9 @@
                                                          'rows'        => '4',
                                                          'required'    => 'required',
                                                          'data-error'  => 'Por favor, escribe un mensaje.' ]) !!}
-                    
+                 <div class="help-block with-errors"></div>    
                 </div>
-                <div class="help-block with-errors"></div>
+               
             </div>
             <div class="col-md-12">
                 <input type="submit" class="btn btn-primary btn-lg btn-send" value="Enviar">
@@ -98,65 +98,7 @@
 
 {!! Form::close() !!}
 
-                    <form class="wow fadeInUp delay2" id="contact-form" method="post" action="contact.php" role="form">
-
-    <div class="messages"></div>
-
-    <div class="controls">
-
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="form_name">Nombre *</label>
-                    <input id="form_name" type="text" name="name" class="form-control" placeholder="Escribe tu nombre aquí *" required="required" data-error="Por favor, ingresa tu nombre.">
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="form_lastname">Apellido *</label>
-                    <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Escribe tu apellido aquí *" required="required" data-error="Por favor, ingresa tu apellido.">
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="form_email">Email *</label>
-                    <input id="form_email" type="email" name="email" class="form-control" placeholder="Escribe tu dirección de correo aquí *" required="required" data-error="Por favor, ingresa una dirección de correo válida.">
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="form_phone">Teléfono</label>
-                    <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Escribe aquí tu número de línea o celular">
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label for="form_message">Mensaje *</label>
-                    <textarea id="form_message" name="message" class="form-control" placeholder="Escribe tu consulta aquí *" rows="4" required="required" data-error="Por favor, escribe un mensaje."></textarea>
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <input type="submit" class="btn btn-success btn-send" value="Enviar">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="text-muted small"><strong>*</strong> Estos campos son obligatorios.</p>
-            </div>
-        </div>
-    </div>
-
-</form>
-
+                   
 
 
 
