8b59e668e35e37e7422780074725aa6aab7eaef4
Syperhugeen <ventas@worldmaster.com.uy>
2016-12-20 23:25:38 -0200

    mail pronto

--- resources/views/emails/solicitud_contacto.blade.php
+++ resources/views/emails/solicitud_contacto.blade.php
@@ -1,12 +1,73 @@
-<br>
+<table width="100%" cellspacing="0" cellpadding="0" border="0">
+ <tbody>
+  <tr>
+   <td bgcolor="#c42450" valign="top" align="center">
+     <table width="600px" cellspacing="0" cellpadding="0" border="0">
+      <tbody>  
 
-Solicitud de contacto de {{$nombre}} ({{$email}} / {{$telefono}})
 
-<br>
 
-<div style="padding: 10px; border-radius: 4px; background-color: #f8f8f8;" >
-  {{$mensaje}}
+<tr height="130px">
+        <td bgcolor="#f1f5e6" valign="top" align="left">
+<table width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+          <tr>
+            <td height="30px">
+            </td>
+          </tr>
+        </tbody>
+        </table>
+
+        <table width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+          <tr>
+            <td height="80px">
+
+<div style="padding-top: 20px; padding-right: 20px; padding-left: 20px; padding-bottom: 40px; font-family:arial; color: #4c4c4c;">
+
+			<h2 style="text-align:center; padding: 5px; border-radius:5px; background-color: #c42450; color: #ffffff;">NUEVO MENSAJE DE CONTACTO</h2>
+
+			<br>
+
+			<b style="color: #c42450;">{{$nombre}}</b> te ha enviado un mensaje desde el <i>formulario de contacto</i>. 
+
+			<br>
+<div style="margin-top: 30px; margin-bottom: 30px;">
+
+			<b>&#8226;</b><i>Datos de contacto:</i>
+
+			<div style="margin-top: 10px;">
+			Direcci&#243;n de correo: <b>{{$email}}</b>
+			<br>
+			Tel&#233;fono: <b>{{$telefono}}</b>
+			</div>
+
+</div>
+			<b>&#8226;</b> <i>Mensaje:</i>
+			<div style="padding: 10px; border-radius: 4px; background-color: #ffffff; color: #808080; font-family:times;" >
+			  {{$mensaje}}
+			</div>
+
+			<br>
+			<br>
+			<hr>
+			<h6 style="text-align:center;">www.evelynjeanette.com</h6>
+
 </div>
 
-<br>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
 
+
+
+
+      </tbody>
+     </table>
+   </td>
+  </tr>
+ </tbody>
+</table>
